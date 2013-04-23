ebf06c57e8fc33bc962495446a199b87c80f9d7d
Ryan Justus <ryanjustus@gmail.com>
2013-04-23 15:40:12 -0600

    Added javadoc comments

--- src/com/screenscraper/datamanager/DataManagerEventSource.java
+++ src/com/screenscraper/datamanager/DataManagerEventSource.java
@@ -14,54 +14,106 @@ import org.apache.commons.logging.LogFactory;
  * @author ryan
  */
 public class DataManagerEventSource {
-    private static Log log = LogFactory.getLog(DataManagerEventSource.class);
-    public enum EventFireTime{
 
-        //called whenever the DataManager creates a new DataNode (DataManager.getNewDataNode)
-        onCreate,
-        //called imediately before the DataNode is written (DataWriter.write),
-        //after all internal data transformations, additions
-        onWrite,
-        //called when values are to be updated in the underlying datasource as opposed
-        //to written as new values
-        onUpdate,
-        //called immediately after the DataNode is written (DataWriter.write),
-        //at this point any values that the write has added are present
-        afterWrite,
-        //called when DataManager.commit is called, after nodes are linked together
-        onCommit,
-        //called whenever DataManager.addData is called
-        onAddData,
-        onInsert,
-        onWriteError;
-    }
+	public enum EventFireTime{
 
-    private Map<EventFireTime, List<DataManagerEventListener>> listeners;
+		/**
+		 * called whenever the DataManager creates a new DataNode (DataManager.getNewDataNode)
+		 */
+		onCreate,
 
-    public DataManagerEventSource(){
-        listeners = new EnumMap<EventFireTime, List<DataManagerEventListener>>(EventFireTime.class);
-        //initialize the map with empty Lists for each valid event fire time
-        for(EventFireTime e: EventFireTime.values()){
-            listeners.put(e, new ArrayList<DataManagerEventListener>());
-        }
-    }
 
-    public synchronized boolean addEventListener(EventFireTime when, DataManagerEventListener listener){
-        return listeners.get(when).add(listener);
-    }
+		/**
+		 * called immediately before the DataNode is written (DataWriter.write),
+		 * after all internal data transformations, additions
+		 */
+		onWrite,
 
-    public synchronized boolean removeEventListener(EventFireTime when, DataManagerEventListener listener){
-        return listeners.get(when).remove(listener);
-    }
 
-    public synchronized List<DataManagerEventListener> getEventListeners(EventFireTime when){
-        return listeners.get(when);
-    }
+		/**
+		 * Called when values are to be updated in the underlying datasource as opposed
+		 * to written as new values
+		 */
+		onUpdate,
 
-    public synchronized void fireEvent(EventFireTime when, DataManagerEvent evt){
-        List<DataManagerEventListener> eventListeners = listeners.get(when);
-        for(DataManagerEventListener listener: eventListeners){
-            listener.handleEvent(evt);
-        }
-    }
+
+		/**
+		 * called immediately after the DataNode is written (DataWriter.write),
+		 * at this point any values that the write has added are present
+		 */
+		afterWrite,
+
+		/**
+		 * called when DataManager.commit is called, after nodes are linked together
+		 */
+		onCommit,
+
+		/**
+		 * called whenever DataManager.addData is called
+		 */
+		onAddData,
+		/**
+		 * Called immediately before a write is performed
+		 */
+		onInsert,
+
+		/**
+		 * Called when the datamanager receives an exception on a write
+		 */
+		onWriteError;
+	}
+
+	private Map<EventFireTime, List<DataManagerEventListener>> listeners;
+
+	/**
+	 *
+	 */
+	public DataManagerEventSource(){
+		listeners = new EnumMap<EventFireTime, List<DataManagerEventListener>>(EventFireTime.class);
+		//initialize the map with empty Lists for each valid event fire time
+		for(EventFireTime e: EventFireTime.values()){
+			listeners.put(e, new ArrayList<DataManagerEventListener>());
+		}
+	}
+
+	/**
+	 * Add a listener
+	 * @param when
+	 * @param listener
+	 * @return
+	 */
+	public boolean addEventListener(EventFireTime when, DataManagerEventListener listener){
+		return listeners.get(when).add(listener);
+	}
+
+	/**
+	 * remove a listener
+	 * @param when
+	 * @param listener
+	 * @return
+	 */
+	public boolean removeEventListener(EventFireTime when, DataManagerEventListener listener){
+		return listeners.get(when).remove(listener);
+	}
+
+	/**
+	 * retrieve all event listeners
+	 * @param when
+	 * @return
+	 */
+	public List<DataManagerEventListener> getEventListeners(EventFireTime when){
+		return listeners.get(when);
+	}
+
+	/**
+	 * Fire event
+	 * @param when
+	 * @param evt
+	 */
+	public void fireEvent(EventFireTime when, DataManagerEvent evt){
+		List<DataManagerEventListener> eventListeners = listeners.get(when);
+		for(DataManagerEventListener listener: eventListeners){
+			listener.handleEvent(evt);
+		}
+	}
 }
